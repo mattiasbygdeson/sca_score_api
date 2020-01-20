1c61506423f1aeef1fb5b7a421bff8329adced54
mabyg1 <Mattias.Bygdeson@cybercom.com>
2020-01-20 13:20:25 +0100

    get, post, delete and put added to api

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const server = express();
+
+const body_parser = require('body-parser');
+
+// parse JSON (application/json content-type)
+server.use(body_parser.json());
+
+const port = 4000;
+
+// << db setup >>
+const db = require("./db");
+const dbName = "sca_game";
+const collectionName = "scoreboard";
+
+// << db init >>
+db.initialize(dbName, collectionName, function(dbCollection) {
+  // Get the entire score list
+  //
+  server.get("/api/score", (request, response) => {
+    dbCollection.find().toArray((error, result) => {
+        if (error) throw error;
+        response.json(result);
+    });
+  });
+
+  // Add new item to score list
+  //
+  server.post("/api/score", (request, response) => {
+    const item = request.body;
+
+    dbCollection.insertOne(item, (error, result) => {
+        if (error) throw error;
+
+        dbCollection.find().toArray((error, result) => {
+            if (error) throw error;
+            response.json(result);
+        });
+    });
+  });
+
+  // Delete a selected item from score list
+  //
+  server.delete("/api/score/:id", (request, response) => {
+    const itemId = request.params._id;
+    console.log("Delete item with id: ", itemId);
+
+    dbCollection.deleteOne({ id: itemId }, function(error, result) {
+        if (error) throw error;
+
+        dbCollection.find().toArray(function(_error, _result) {
+            if (_error) throw _error;
+            response.json(_result);
+        });
+    });
+
+  // Update a selected item with new data
+  //
+  server.put("/api/score/:id", (request, response) => {
+    const itemId = request.params._id;
+    const item = request.body;
+    console.log("Editing item: ", itemId, " to be ", item);
+
+    dbCollection.updateOne({ id: itemId }, { $set: item }, (error, result) => {
+        if (error) throw error;
+
+        dbCollection.find().toArray(function(_error, _result) {
+            if (_error) throw _error;
+            response.json(_result);
+        });
+    });
+  });
+});
+
+}, function(err) {
+  throw (err);
+});
+
+// << db init >>
+server.listen(port, () => {
+  console.log(`Server listening at ${port}`);
+});
